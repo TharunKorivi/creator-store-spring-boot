f408e789b597f77d1f639be5698a083671ed5d35
TharunKorivi <tharun.korivi04@gmail.com>
2026-05-18 00:37:56 +0530

    First commit

---
+++ src/test/java/com/tharunkorivi/creatorstore/CreatorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharunkorivi.creatorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
